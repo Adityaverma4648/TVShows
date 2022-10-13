d92402f5da1e26f481cb4773b86e670b76fe019a
Aditya verma <87409406+Adityaverma4648@users.noreply.github.com>
2022-10-13 22:38:32 +0530

    removed react

--- App1/TVShows/urls.py
+++ App1/TVShows/urls.py
@@ -2,13 +2,12 @@
 from django.contrib import admin
 from django.urls import path , include
 from TVShows import views
+# from tv_shows import 
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('',views.index ,name = 'home'),
-    path('about',views.index ,name = 'about'),
-    path('contacts',views.index ,name = 'contacts'),
-    path('services',views.index ,name = 'services'),
-
-
+    path('about',views.about ,name = 'about'),
+    path('contacts',views.contacts ,name = 'contacts'),
+    path('services',views.services,name = 'services'),
 ]
--- App1/templates/index.js
+++
@@ -1,4 +0,0 @@
-import * as data from './tv_shows.csv';
-
-const {Year} = data;
-console.log(Year)
