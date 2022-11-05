94e7b7d304472eb9c20b6f51a992948324b0d17f
Aditya verma <87409406+Adityaverma4648@users.noreply.github.com>
2022-11-05 18:07:16 +0530

    integrated React and Django

--- App1/App1/settings.py
+++ App1/App1/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'App1'
+    # own
+    'App1', # dynamic
+    'TVShows.apps.TvshowsConfig',
+    
 ]
 
 MIDDLEWARE = [
